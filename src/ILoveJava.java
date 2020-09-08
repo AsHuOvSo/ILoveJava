ac35720e69be4235eb40e873e434f36d3fa15b58
AsHuOvSo <asta@avra.dk>
2020-09-08 10:56:43 +0200

    Initial commit

---
+++ src/ILoveJava.java
@@ -0,0 +1,10 @@
+public class ILoveJava {
+    //Starting line
+    public static void main(String[] args) {
+        //for loop - makes it easier to do a repetitive cycle
+        for(int i = 0; i < 5; i++) {
+            //prints out I Love Java on 5 lines
+            System.out.println("I Love Java!");
+        }
+    }
+}
